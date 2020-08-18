3aeef09024aad42df9695f36a5fb748e4e81cbff
doakcovington <doakcovington@protonmail.com>
2020-08-18 11:29:46 -0700

    Connected to server

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const { request } = require('http');
 const { response } = require('express');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 
 app.get('/', (request, response) => response.send("We Don't Go To Ravenholm"));
 
-const PORT = porcess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+const sequelize = new Sequelize('pulse_ox', 'postgres', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+sequelize.authenticate()
+.then(() => console.log('Database connected...'))
+.catch(error => console.log('Error: ' + error)) 
